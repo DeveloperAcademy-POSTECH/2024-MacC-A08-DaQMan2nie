cf2978b15d196640376afdc9d791cd1a534bd4cc
pil_gaaang <21900204@handong.ac.kr>
2024-10-05 21:38:01 +0900

    feat: workingVeiw 페이지 추가, 페이지 전환 기능 추가

--- hearo/hearo/Sources/Presentations/Splash/View/SplashView.swift
+++ hearo/hearo/Sources/Presentations/Splash/View/SplashView.swift
@@ -4,15 +4,33 @@
 //
 //  Created by Pil_Gaaang on 10/4/24.
 //
-
 import SwiftUI
 
 struct SplashView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @ObservedObject var appRootManager: AppRootManager
+    @State private var isActive = false // 스플래시 화면이 끝났는지 여부를 관리하는 상태
 
-#Preview {
-    SplashView()
+    var body: some View {
+        VStack {
+            if isActive {
+                // 상태를 즉시 변경하여 OnboardingView로 전환
+                // EmptyView를 사용하지 않고 상태만 변경
+                Color.clear
+                    .onAppear {
+                        appRootManager.currentRoot = .onboarding // 상태 변경
+                    }
+            } else {
+                // 스플래시 화면의 내용 (예: 로고 또는 이미지)
+                Text("스플래시")
+                    .font(.largeTitle)
+                    .bold()
+                    .onAppear {
+                        // 2초 후 isActive 상태를 true로 변경
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            self.isActive = true
+                        }
+                    }
+            }
+        }
+    }
 }
--- hearo/hearo/Sources/Presentations/Splash/ViewModel/SplashViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SplashViewModel.swift
-//  hearo
-//
-//  Created by Pil_Gaaang on 10/4/24.
-//
-
-import Foundation
