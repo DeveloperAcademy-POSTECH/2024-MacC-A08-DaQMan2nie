e93b3457837052e253b876c3cf014e1ca3f2100b
pil_gaaang <21900204@handong.ac.kr>
2024-10-10 19:48:30 +0900

    [feat] working viw

--- hearo/hearo/Sources/Presentations/Home/View/HomeView.swift
+++ hearo/hearo/Sources/Presentations/Home/View/HomeView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct HomeView: View {
     @ObservedObject var appRootManager: AppRootManager
-
+    @State private var currentDate: String = ""
+    
     var body: some View {
         VStack {
             Spacer().frame(height: 89)
@@ -32,7 +33,7 @@ struct HomeView: View {
             HStack{
                 Spacer().frame(width: 16)
                 
-                Text("2024. 09. 22  화요일")
+                Text(currentDate)
                     .font(Font.custom("Spoqa Han Sans Neo", size: 18))
                     .foregroundColor(Color("HGray2"))
                     .frame(width: 295, alignment: .topLeading)
@@ -73,5 +74,11 @@ struct HomeView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(red: 28/255, green: 34/255, blue: 46/255, opacity: 1))
         .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "ko_KR")
+            dateFormatter.dateFormat = "yyyy. MM. dd. EEEE"
+            currentDate = dateFormatter.string(from: Date()) 
+        }
     }
 }
