1b065346e1daac4bd33d8c2d22eb275c959c24b8
pil_gaaang <21900204@handong.ac.kr>
2024-10-10 19:52:59 +0900

    [feat] finish view

--- hearo/hearo/Sources/Presentations/Finish/View/FinishView.swift
+++ hearo/hearo/Sources/Presentations/Finish/View/FinishView.swift
@@ -12,19 +12,64 @@ struct FinishView: View {
 
     var body: some View {
         VStack {
-            Text("피니시 화면")
-                .font(.largeTitle)
-                .padding()
-
-            // 홈 화면으로 돌아가는 버튼
-            Button("홈 화면으로 돌아가기") {
-                print("홈 화면 버튼")
-                appRootManager.currentRoot = .home // 홈 화면으로 전환
+            Spacer().frame(height: 84)
+            
+            HStack{
+                Spacer().frame(width: 16)
+                
+                Text("안전 주행 완료!")
+                    .font(
+                        Font.custom("Spoqa Han Sans Neo", size: 25)
+                            .weight(.bold)
+                    )
+                    .foregroundColor(Color("HWhite"))
+                
+                Spacer()
             }
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(10)
+            
+            Spacer().frame(height: 19)
+            
+            HStack{
+                Spacer().frame(width: 16)
+                
+                Text("오늘도 히어로드와 함께 무사히 도착하셨습니다.\n다음에도 안전하게 뵙겠습니다!")
+                    .font(Font.custom("Spoqa Han Sans Neo", size: 16))
+                    .foregroundColor(Color("HGray2"))
+                    .frame(width: 295, alignment: .topLeading)
+                
+                Spacer()
+            }
+            
+            Spacer().frame(height: 104)
+            
+            Image(systemName: "checkmark.circle.fill") // SF Symbols에서 원형 아이콘 사용
+                .resizable()
+                .frame(width: 160, height: 160)
+                .foregroundColor(Color("HPrimaryColor")) // 아이콘 색상 설정
+            
+            
+            Spacer().frame(height: 231)
+            Button(action: {
+                appRootManager.currentRoot = .home
+            }) {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 361, height: 58)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .opacity(0.28)
+                    
+                    Text("시작하기")
+                        .font(Font.custom("Spoqa Han Sans Neo", size: 18).weight(.medium))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                }
+            }
+            Spacer()
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(red: 28/255, green: 34/255, blue: 46/255, opacity: 1))
+        .edgesIgnoringSafeArea(.all)
     }
 }
