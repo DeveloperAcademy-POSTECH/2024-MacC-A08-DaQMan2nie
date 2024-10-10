cf72524dbdcb13654ff15a063ded9e9a638a1572
pil_gaaang <21900204@handong.ac.kr>
2024-10-10 19:28:07 +0900

    [feat] onboarding page 3

--- hearo/hearo/Sources/Presentations/OnBoarding/View/OnboardingView.swift
+++ hearo/hearo/Sources/Presentations/OnBoarding/View/OnboardingView.swift
@@ -140,12 +140,12 @@ struct OnboardingView: View {
                 Spacer().frame(height: 121.28)
                 
                 HStack(spacing: 4) {
-                                ForEach(1..<4) { index in // 2, 3, 4 페이지 인디케이터
-                                    Circle()
-                                        .fill(currentPage == index ? Color.white : Color.gray) // 현재 페이지에 따라 색상 조정
-                                        .frame(width: 8, height: 8)
-                                }
-                            }
+                    ForEach(1..<4) { index in // 2, 3, 4 페이지 인디케이터
+                        Circle()
+                            .fill(currentPage == index ? Color.white : Color.gray) // 현재 페이지에 따라 색상 조정
+                            .frame(width: 8, height: 8)
+                    }
+                }
                 Spacer().frame(height: 51)
                 
                 Button(action: {
@@ -169,7 +169,7 @@ struct OnboardingView: View {
                 Spacer()
             }
             .tag(1)
-
+            
             // 세 번째 온보딩 페이지
             VStack {
                 Spacer().frame(height: 59)
@@ -188,14 +188,14 @@ struct OnboardingView: View {
                 }
                 
                 Spacer().frame(height: 19)
-
+                
                 HStack{
                     Spacer().frame(width: 16)
                     
                     Text("녹음된 오디오는 오직 경적 소리 인식 목적으로만 \n사용됩니다.\n\n녹음 데이터는 저장되지 않으며, 실시간으로 분석 후 즉시 삭제됩니다. \n\n사용자의 다른 행동이나 대화는 절대 녹음되지 않습니다.")
-                      .font(Font.custom("Spoqa Han Sans Neo", size: 14))
-                      .foregroundColor(Color(red: 0.7, green: 0.7, blue: 0.7))
-                      .frame(width: 345, alignment: .topLeading)
+                        .font(Font.custom("Spoqa Han Sans Neo", size: 14))
+                        .foregroundColor(Color(red: 0.7, green: 0.7, blue: 0.7))
+                        .frame(width: 345, alignment: .topLeading)
                     
                     Spacer()
                 }
@@ -210,12 +210,12 @@ struct OnboardingView: View {
                 Spacer().frame(height: 110)
                 
                 HStack(spacing: 4) {
-                                ForEach(1..<4) { index in // 2, 3, 4 페이지 인디케이터
-                                    Circle()
-                                        .fill(currentPage == index ? Color.white : Color.gray) // 현재 페이지에 따라 색상 조정
-                                        .frame(width: 8, height: 8)
-                                }
-                            }
+                    ForEach(1..<4) { index in // 2, 3, 4 페이지 인디케이터
+                        Circle()
+                            .fill(currentPage == index ? Color.white : Color.gray) // 현재 페이지에 따라 색상 조정
+                            .frame(width: 8, height: 8)
+                    }
+                }
                 Spacer().frame(height: 51)
                 
                 Button(action: {
@@ -237,11 +237,74 @@ struct OnboardingView: View {
                 }
                 
                 Spacer()
-                Spacer()
                 
             }
             .tag(2)
+            Spacer().frame(height: 59)
             
+            HStack{
+                Spacer().frame(width: 19)
+                
+                Text("경적 소리 감지시\n안전을 위한 알림을 허용하세요")
+                    .font(
+                        Font.custom("Spoqa Han Sans Neo", size: 25)
+                            .weight(.bold)
+                    )
+                    .foregroundColor(Color("HWhite"))
+                
+                Spacer()
+            }
+            
+            Spacer().frame(height: 19)
+            
+            HStack{
+                Spacer().frame(width: 16)
+                
+                Text("녹음된 오디오는 오직 경적 소리 인식 목적으로만 \n사용됩니다.\n\n녹음 데이터는 저장되지 않으며, 실시간으로 분석 후 즉시 삭제됩니다. \n\n사용자의 다른 행동이나 대화는 절대 녹음되지 않습니다.")
+                    .font(Font.custom("Spoqa Han Sans Neo", size: 14))
+                    .foregroundColor(Color(red: 0.7, green: 0.7, blue: 0.7))
+                    .frame(width: 345, alignment: .topLeading)
+                
+                Spacer()
+            }
+            
+            Spacer().frame(height: 64)
+            
+            Image(systemName: "lock.shield.fill")
+                .resizable()
+                .frame(width: 122.39725, height: 164.59821)
+                .foregroundColor(Color("HPrimaryColor"))
+            
+            Spacer().frame(height: 110)
+            
+            HStack(spacing: 4) {
+                ForEach(1..<4) { index in // 2, 3, 4 페이지 인디케이터
+                    Circle()
+                        .fill(currentPage == index ? Color.white : Color.gray) // 현재 페이지에 따라 색상 조정
+                        .frame(width: 8, height: 8)
+                }
+            }
+            Spacer().frame(height: 51)
+            
+            Button(action: {
+                currentPage = 3
+            }) {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 361, height: 58)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .opacity(0.28)
+                    
+                    Text("확인")
+                        .font(Font.custom("Spoqa Han Sans Neo", size: 18).weight(.medium))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                }
+            }
+            
+            Spacer()
             // 네 번째 온보딩 페이지
             VStack {
                 Text("온보딩 4 번째 페이지")
