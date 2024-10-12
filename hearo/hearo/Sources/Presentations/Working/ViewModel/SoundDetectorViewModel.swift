397ce929725c5ac2d991ebed8f2b5081af9ba9ee
strlla4536 <strlla4536@gmail.com>
2024-10-13 00:49:55 +0900

    [feat] 소리 감지 뷰모델 생성 - 사용자가 "시작" 버튼을 누르면 녹음 시작 - 소리를 감지하면 알림 발생

---
+++ hearo/hearo/Sources/Presentations/Working/ViewModel/SoundDetectorViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  SoundDetectorViewModel.swift
+//  hearo
+//
+//  Created by 규북 on 10/13/24.
+//
+
+import Foundation
+import Combine
+
+class SoundDetectorViewModel: ObservableObject {
+    @Published var isRecording = false
+    @Published var classificationResult = "녹음 시작 전"
+    @Published var detectedHornSound = false
+    
+    private let soundDetector = BicycleHornSoundDetector()
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        setupBindings()
+    }
+    
+    private func setupBindings() {
+        soundDetector.$isRecording
+            .assign(to: \.isRecording, on: self)
+            .store(in: &cancellables)
+        
+        soundDetector.$classificationResult
+            .assign(to: \.classificationResult, on: self)
+            .store(in: &cancellables)
+        
+        soundDetector.$detectedHornSound
+            .assign(to: \.detectedHornSound, on: self)
+            .store(in: &cancellables)
+    }
+    
+    func startRecording() {
+        soundDetector.startRecording()
+    }
+    
+    func stopRecording() {
+        soundDetector.stopRecording()
+    }
+}
+
