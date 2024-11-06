03c208767fec6a9b50eeecdb5ca2402448a57bd3
strlla4536 <strlla4536@gmail.com>
2024-11-06 18:32:11 +0900

    SplashView UI 작업

--- hearo/hearo/Sources/Presentations/Splash/View/SplashView.swift
+++ hearo/hearo/Sources/Presentations/Splash/View/SplashView.swift
@@ -23,43 +23,26 @@ struct SplashView: View {
               appRootManager.determineNextRoot() // splash 후 다음 화면 결정
             }
         } else {
-          
           Spacer().frame(height: 217)
           
-          Text("Hearoad")
-            .font(Font.custom("Inter", size: 34.80519)
-              .weight(.medium)
-            )
-            .multilineTextAlignment(.center)
-            .foregroundColor(Color("HWhite"))
+          Image("Hearoad_Main")
+            .resizable()
+            .frame(width: 139.55, height: 25.68,alignment: .center)
           
           Spacer()
           
-          HStack(spacing: 0) {
-            
-            Text("Copyright 2024.")
-              .font(Font.custom("Spoqa Han Sans Neo", size: 12))
-              .multilineTextAlignment(.center)
-              .foregroundColor(Color("HWhite"))
-            Text(" DaQman2ni")
-              .font(Font.custom("Spoqa Han Sans Neo-Bold", size: 12))
-              .multilineTextAlignment(.center)
-              .foregroundColor(Color("HWhite"))
-            Text(" in all rights reserved.")
-              .font(Font.custom("Spoqa Han Sans Neo", size: 12))
-              .multilineTextAlignment(.center)
-              .foregroundColor(Color("HWhite"))
-            
-          }
-          .padding(.bottom, 44)
+          Image("Copyright")
+            .resizable()
+            .frame(width: 246.24, height: 10.53, alignment: .center)
+            .padding(.bottom, 46.69)
           
         }
         
       }
       .onAppear {
-        // 2초 후 isActive 상태를 true로 변경
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-          self.isActive = true
+        // 1.5초 후 isActive 상태를 true로 변경
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+//          self.isActive = true
         }
       }
     }
