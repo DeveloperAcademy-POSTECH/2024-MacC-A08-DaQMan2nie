919ac2ca144c9bb55f3f257bcfe2dfd5d5f6c15c
strlla4536 <strlla4536@gmail.com>
2024-11-06 21:09:12 +0900

    [fix] git - resolve conflicts

--- hearo/hearo/Sources/Presentations/Splash/View/SplashView.swift
+++ hearo/hearo/Sources/Presentations/Splash/View/SplashView.swift
@@ -31,44 +31,21 @@ struct SplashView: View {
           
           Spacer()
           
-<<<<<<< HEAD
+
           Image("Copyright")
             .resizable()
             .frame(width: 246.24, height: 10.53, alignment: .center)
             .padding(.bottom, 46.69)
-=======
-          HStack(spacing: 0) {
-            
-            Text("Copyright 2024.")
-              .font(Font.custom("Spoqa Han Sans Neo", size: 12))
-              .multilineTextAlignment(.center)
-              .foregroundColor(Color("HWhite"))
-            Text(" DaQman2ni")
-              .font(Font.custom("Spoqa Han Sans Neo-Bold", size: 12))
-              .multilineTextAlignment(.center)
-              .foregroundColor(Color("HWhite"))
-            Text(" in all rights reserved.")
-              .font(Font.custom("Spoqa Han Sans Neo", size: 12))
-              .multilineTextAlignment(.center)
-              .foregroundColor(Color("HWhite"))
-            
-          }
-          .padding(.bottom, 44)
->>>>>>> f8ee93d (fix UI)
+
           
         }
         
       }
       .onAppear {
-<<<<<<< HEAD
+
         // 1.5초 후 isActive 상태를 true로 변경
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
 //          self.isActive = true
-=======
-        // 2초 후 isActive 상태를 true로 변경
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-          self.isActive = true
->>>>>>> f8ee93d (fix UI)
         }
       }
     }
