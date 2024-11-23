1a965ffb25ab130921841f16d5fc36ebc83b0151
strlla4536 <strlla4536@gmail.com>
2024-11-24 00:07:03 +0900

    [UI] WarningView 수정했습니다

--- hearo/hearo/Sources/Presentations/Warning/WarningView/WarningView.swift
+++ hearo/hearo/Sources/Presentations/Warning/WarningView/WarningView.swift
@@ -9,23 +9,38 @@ import SwiftUI
 struct WarningView: View {
     @ObservedObject var viewModel: WarningViewModel
 
-    var body: some View {
-        VStack {
-            // 감지된 소리에 따라 알맞은 이미지를 표시
-            Image(viewModel.alertImageName())
-                .resizable()
-                .scaledToFit()
-                .frame(width: 400, height: 400)
-                .padding()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.red)
-        .edgesIgnoringSafeArea(.all)
-        .onAppear {
-            // 3초 후 자동으로 WorkingView로 전환
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                viewModel.appRootManager.currentRoot = .working
-            }
-        }
+  var body: some View {
+    ZStack(alignment: .center) {
+      LottieView(animationName: "warning_wave", animationScale: 1, loopMode: .loop)
+        .frame(alignment: .center)
+        .offset(y:60)
+        
+      VStack(spacing:0) {
+        
+        // 감지된 소리에 따라 알맞은 텍스트를 표시
+        Image("\(viewModel.alertImageName())-text")
+          .padding(.top, 157)
+          .padding(.bottom, 80)
+        
+        // 감지된 소리에 따라 알맞은 이미지를 표시
+        Image(viewModel.alertImageName())
+
+        Spacer()
+      }
     }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(Color("WarningColor"))
+    .edgesIgnoringSafeArea(.all)
+    .onAppear {
+      // 3초 후 자동으로 WorkingView로 전환
+      DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+        viewModel.appRootManager.currentRoot = .working
+      }
+    }
+  
+    }
+}
+
+#Preview {
+  WarningView(viewModel: WarningViewModel(appRootManager: AppRootManager()))
 }
