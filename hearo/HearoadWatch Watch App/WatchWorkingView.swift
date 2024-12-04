bb7680d6307029bfad35d5b10d2152fc1a409e5f
chosun-sangnom <163488928+chosun-sangnom@users.noreply.github.com>
2024-12-05 08:36:25 +0900

    워치 워킹뷰 애니메이션 구현

--- hearo/HearoadWatch Watch App/WatchWorkingView.swift
+++ hearo/HearoadWatch Watch App/WatchWorkingView.swift
@@ -8,20 +8,43 @@
 import SwiftUI
 
 struct WatchWorkingView: View {
+    @State private var currentImageIndex = 0
+    let images: [UIImage] = {
+        var loadedImages = [UIImage]()
+        for i in 0...105 {
+            if let image = UIImage(named: "30049edb3b6349defd49d650efeacf33EfCf9qvpQqqmGXY4-\(i)") {
+                loadedImages.append(image)
+            }
+        }
+        return loadedImages
+    }()
+    
     var body: some View {
-        ZStack{
-            Color("Radish")
+        ZStack {
+            Color(.white)
                 .ignoresSafeArea(.all)
+            
             Text("소리 수집중")
                 .font(Font.custom("Pretendard", size: 26).weight(.medium))
                 .foregroundColor(Color("SubFontColor"))
                 .multilineTextAlignment(.center)
                 .offset(y:-90)
-            Image("WorkingViewCircle")
+            
+            Image(uiImage: images[currentImageIndex])
                 .resizable()
                 .scaledToFit()
                 .frame(width: 160, height: 160, alignment: .center)
                 .offset(y:10)
+                .onAppear {
+                    animateImages()
+                }
+        }
+    }
+    
+    private func animateImages() {
+        // Timer를 사용하여 이미지 시퀀스를 변경
+        Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { timer in
+            currentImageIndex = (currentImageIndex + 1) % images.count
         }
     }
 }
